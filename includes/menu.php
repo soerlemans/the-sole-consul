777e48f3d084d6d4bad185da2f304522da091325
sappigeninja <sebasoerlemans@gmail.com>
2021-04-29 01:44:34 +0200

    Pi overheated, last coding session for website

--- includes/menu.php
+++ includes/menu.php
@@ -8,21 +8,22 @@
 		
 		$regexp = "/<h2.*>.*<\/h2>/";
 		if(preg_match_all($regexp, file_get_contents($current_file), $matches))
-		{
 			foreach($matches[0] as $match)
-			{
-				echo "<li> <a href='#";
-				// For this to work an id must be set
-				preg_match_all('/id="[a-z]*"/', $match, $ids);
-				$stripped_front = preg_replace('/id="/', "", $ids[0][0]);
-				$stripped = preg_replace('/"/', "", $stripped_front);
-				echo $stripped;
-				echo "'>";
-				
-				echo preg_replace("/<\/?h2[^>]*>/", "", $match);
+		{
+			echo "<li> <a href='#";
 
-				echo "</a> </li>";
-			}
+			// For this to work an id must be set
+			preg_match_all('/id="[a-z]*"/', $match, $ids);
+			
+			$stripped_front = preg_replace('/id="/', "", $ids[0][0]);
+			$stripped = preg_replace('/"/', "", $stripped_front);
+			echo $stripped;
+			
+			echo "'>";
+			
+			echo preg_replace("/<\/?h2[^>]*>/", "", $match);
+
+			echo "</a> </li>";
 		}
 		?>
 	</ul>
